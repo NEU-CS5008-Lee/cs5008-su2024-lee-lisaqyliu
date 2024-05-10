543c1cef133f88dd71ab32dcd676b9f313fa9fe9
lisaqyliu <lisaliu9194@gmail.com>
2024-05-09 23:48:07 -0700

    upload assignment 1

---
+++ helloworld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// name: Qingyang Liu 
+// email: liu.qingyan@northeastern.edu
+
+int main() {
+  printf("Hello World.  This is Qingyang Liu.\n");
+  printf("I am assignment 1 for Summer 2024.\n");
+  return 0;
+}
