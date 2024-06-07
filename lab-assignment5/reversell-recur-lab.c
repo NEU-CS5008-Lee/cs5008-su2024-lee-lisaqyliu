e496a92f50c3e2f8d65b7f85373051a79b7a4404
lisaqyliu <lisaliu9194@gmail.com>
2024-06-07 10:48:35 -0700

    Upload lab5

--- lab-assignment5/reversell-recur-lab.c
+++ lab-assignment5/reversell-recur-lab.c
@@ -1,4 +1,5 @@
-//enter your name and email here
+//Qingyang Liu
+// liu.qingyan@northeastern.edu
 #include<stdio.h>
 #include<stdlib.h>
 typedef struct node
@@ -14,15 +15,30 @@ typedef struct list{
 /*-----creating the nodes----------*/
 node_t* newNode(int num)
 {
+    node_t* new_node = (node_t*)malloc(sizeof(node_t));
+    new_node->data = num;
+    new_node->next = NULL;
+    return new_node;
  
 }
 /*---creating linked list----*/
 List* init_LL(){
+    List* new_list = (List*)malloc(sizeof(List));
+    new_list->head = NULL;
+    return new_list;
     
 }
 
 /*---Insert the nodes at the begining of the list---*/
 void insertFirst(List* l, int data){
+    node_t* new_node = newNode(data);
+    if(l->head==NULL){
+        l->head = new_node;
+        return;
+    }
+    new_node->next = l->head;
+    l->head = new_node;
+    return;
     
 
 }
@@ -48,7 +64,16 @@ void reverse(List* l, node_t* ptr)
     
     
 //insert your code here
-    
+    // Base case: if the list is empty or has only one node
+    if ((ptr == NULL || ptr->next == NULL)) {
+        l->head = ptr;
+        return;
+    }
+    node_t* next_node = ptr->next;
+    reverse(l, next_node);
+    next_node->next = ptr;
+    ptr->next = NULL;
+    return;
     
 }
 
