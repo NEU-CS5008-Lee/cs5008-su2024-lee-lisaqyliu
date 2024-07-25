9b0015e7220236ed65d7cdfd3c9bd6ea63910228
lisaqyliu <lisaliu9194@gmail.com>
2024-07-24 22:39:01 -0700

    Upload assignment 11

--- assignment11/hw11a.c
+++ assignment11/hw11a.c
@@ -1,5 +1,5 @@
-// name: <your name here>
-// email: <your email here>
+// name: Qingyang Liu
+// email: liu.qingyan@northeastern.edu
 
 #include <stdio.h>
 
@@ -8,6 +8,15 @@ int d[20];
 long long int dp(int n) {
 
     // Add your code here
+    d[0] = 1; // One way to do nothing(tile an empty floor)
+    d[1] = 1; // One way to place a single vertical tile
+
+    // Compute values from 2 to n
+    for (int i = 2; i <= n; i++) {
+        d[i] = d[i - 1] + d[i - 2];
+    }
+
+    return d[n];
 
 }
 
--- assignment11/hw11c.c
+++ assignment11/hw11c.c
@@ -1,5 +1,5 @@
-// name: <your name here>
-// email: <your email here>
+// name: Qingyang Liu
+// email: liu.qingyan@northeastern.edu
 
 #include <stdio.h>
 
@@ -8,6 +8,20 @@ int d[20];
 long long int dp(int n) {
 
     // Add your code here
+    // base case, one way to do nothing(tile an empty floor)
+    d[0] = 1;
+    if (n >= 1) d[1] = 0; // One way to place a single vertical tile
+    if (n >= 2) d[2] = 3; // Three ways to place a 2x2 square
+    if (n >= 3) d[3] = 0; // No ways to place a 2x3 rectangle
+
+    // Compute values from 4 to n
+    for (int i = 4; i <= n; i++) { 
+        d[i] = 3 * d[i - 2]; // Add the number of ways to place a 2x1 vertical tile
+        for (int j = 4; j <= i; j += 2){
+            d[i] += 2 * d[i - j]; // Add the number of ways to place a 2x2 square
+        }
+    }
+    return d[n];
 
 }
 
