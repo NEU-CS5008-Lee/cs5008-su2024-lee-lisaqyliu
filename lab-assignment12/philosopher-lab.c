97d360822178009016d103c25537d2066d328d1c
lisaqyliu <lisaliu9194@gmail.com>
2024-08-01 01:00:08 -0700

    Upload lab 12

--- lab-assignment12/philosopher-lab.c
+++ lab-assignment12/philosopher-lab.c
@@ -1,10 +1,10 @@
-//Enter your name here
-//Enter your email here
+//Qingyang Liu
+//liu.qingyan@northeastern.edu
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
 pthread_mutex_t chopstick[5];//lock variable
-
+// Mutex: at most one threat may own the lock. When thread A is holding the lock, no other thread can acquire the lock.
 // Thread to be executed
 void *philosopher(void *x)
 {
@@ -13,7 +13,30 @@ void *philosopher(void *x)
     int n=*a;
     
     /*-----Insert your code here----*/
-    
+    // Think
+    printf("Philosopher %d is thinking.\n", n);
+    sleep(1); // Simulate thinking
+
+    // Pick up chopsticks in order
+    if (n == 4) { // Last philosopher picks right first, then left
+        pthread_mutex_lock(&chopstick[(n+1)%5]); // Right chopstick
+        printf("Philosopher %d is eating using chopstick[%d] and chopstick[%d].\n", n, (n+1)%5, n);
+        pthread_mutex_lock(&chopstick[n]);       // Left chopstick
+    } else { // All other philosophers pick left first, then right
+        pthread_mutex_lock(&chopstick[n]);       // Left chopstick
+        pthread_mutex_lock(&chopstick[(n+1)%5]); // Right chopstick
+        printf("Philosopher %d is eating using chopstick[%d] and chopstick[%d].\n", n, n, (n+1)%5);
+    }
+
+    // Eat
+    sleep(1); // Simulate eating
+
+    // Put down chopsticks
+    pthread_mutex_unlock(&chopstick[n]);             // Left chopstick
+    pthread_mutex_unlock(&chopstick[(n+1)%5]);       // Right chopstick
+
+    printf("Philosopher %d finished eating.\n", n);
+    return NULL; // Return from the thread after one cycle
     
 }
 
