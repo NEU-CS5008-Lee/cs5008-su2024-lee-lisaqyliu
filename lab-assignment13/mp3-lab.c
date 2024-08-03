4ea64c1c5f6b03fb1217cd32c3c4db66b2b8b352
lisaqyliu <lisaliu9194@gmail.com>
2024-08-02 21:52:54 -0700

    Upload lab 13

--- lab-assignment13/mp3-lab.c
+++ lab-assignment13/mp3-lab.c
@@ -1,5 +1,5 @@
-//enter your email here
-//enter your name here
+//liu.qingyan@northeastern.edu
+//Qingyang Liu
 
 #include<stdio.h>
 #include<string.h>
@@ -10,6 +10,28 @@ int compression(char arr[], int n, char res[]){
     
     
     //insert your code here
+    int count=1; //initialize the count of the first character
+    int j=0; //initialize the index of the result array
+    for(int i=0;i<n;i++){
+        if(i < n - 1 && arr[i]==arr[i + 1]){
+            count++;
+        }else{ 
+            //add the character to the result array
+            res[j++]=arr[i];
+            // Only add the count if its greater than 1
+            if(count>1){
+                //convert the count to a string and add it to the result array
+                char str[10];
+                sprintf(str,"%d",count);
+                for(int k=0;str[k]!='\0';k++){
+                    res[j++]=str[k];
+                }
+            }
+            count=1;
+        }
+    }
+    res[j]='\0'; //add the null character to the end of the result array
+    return j; //return the length of the result array
     
     
     
